8744a66e2769c8a38b387e9e33b55dc1df193835
Luciano <lucianux@gmail.com>
2021-12-24 16:49:07 -0300

    Initialization

---
+++ entity_framework_core_memory/exampleEntityFrameworkCoreMemory/DataContext/Data/DbInitializer.cs
@@ -0,0 +1,74 @@
+using exampleEntityFrameworkCoreMemory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace exampleEntityFrameworkCoreMemory.DataContext.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var _context = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                // Adding Artists to DB
+                if (_context.Artists.Any())
+                {
+                    return;
+                }
+
+                _context.Artists.AddRange(
+                    new Artist { Name = "Luis Miguel" },
+                    new Artist { Name = "Ricardo Arjona" },
+                    new Artist { Name = "Kalimba" }
+                 );
+
+                _context.SaveChanges();
+
+                // Adding Albums to DB
+                if (_context.Albums.Any())
+                {
+                    return;
+                }
+
+                _context.Albums.AddRange(
+                    new Album
+                    {
+                        Id = _context.Artists.FirstOrDefault(
+                            a => a.Name.Equals("Kalimba")).Id,
+                        Title = $"Mi Otro Yo",
+                        Price = 200
+                    },
+
+                    new Album
+                    {
+                        Id = _context.Artists.FirstOrDefault(
+                            a => a.Name.Equals("Kalimba")).Id,
+                        Title = $"Aerosoul",
+                        Price = 275
+                    },
+
+                    new Album
+                    {
+                        Id = _context.Artists.FirstOrDefault(
+                            a => a.Name.Equals("Ricardo Arjona")).Id,
+                        Title = $"Circo Soledad",
+                        Price = 180
+                    },
+
+                    new Album
+                    {
+                        Id = _context.Artists.FirstOrDefault(
+                            a => a.Name.Equals("Luis Miguel")).Id,
+                        Title = $"Romance",
+                        Price = 290
+                    }
+                );
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
