79939f739bed57eafaefb9be763ba045d5ca61cb
Chuzas <lucianux@gmail.com>
2022-10-29 16:21:45 -0300

    Uploading program example

---
+++ docker/exampleApp/exampleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
